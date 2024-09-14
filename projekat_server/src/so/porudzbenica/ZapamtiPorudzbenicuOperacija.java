32e79f59f8572071ab4d6de175f797879dc4c6df
Mina Kovacina <softverimina@gmail.com>
2024-09-14 17:54:43 +0200

    Diplomski - inicijalni projekat - zajednicki

--- projekat_server/src/so/porudzbenica/ZapamtiPorudzbenicuOperacija.java
+++ projekat_server/src/so/porudzbenica/ZapamtiPorudzbenicuOperacija.java
@@ -13,11 +13,11 @@ import so.ApstraktnaOperacija;
  *
  * @author Mina
  */
-public class ZapamtiPorudzbenicuOperacija extends ApstraktnaOperacija{
+public class ZapamtiPorudzbenicuOperacija extends ApstraktnaOperacija {
 
     @Override
     protected void validiraj(Object podatak) throws Exception {
-        if(!(podatak instanceof Porudzbenica)){
+        if (!(podatak instanceof Porudzbenica)) {
             throw new Exception("Prosledjeni objekat nije klase Porudzbenica");
         }
     }
@@ -25,37 +25,36 @@ public class ZapamtiPorudzbenicuOperacija extends ApstraktnaOperacija{
     @Override
     protected Object izvrsi(Object podatak) throws Exception {
         Porudzbenica novaPorudzbenica = (Porudzbenica) podatak;
-        
+
         try {
             DatabaseBroker.getInstanca().update(novaPorudzbenica);
-        
-        Porudzbenica staraPorudzebica = (Porudzbenica) DatabaseBroker.getInstanca().findOne(novaPorudzbenica);
-        
-        for(StavkaPorudzbenice stavka: staraPorudzebica.getStavke()){
-            if(!novaPorudzbenica.getStavke().contains(stavka)){
-                DatabaseBroker.getInstanca().delete(stavka);
-            }
-        }
-        
-        for(StavkaPorudzbenice stavka: novaPorudzbenica.getStavke()){
-            if(!staraPorudzebica.getStavke().contains(stavka)){
-                DatabaseBroker.getInstanca().insert(stavka);
-            }else{
-                int index = staraPorudzebica.getStavke().indexOf(stavka);
-                StavkaPorudzbenice staraStavka = staraPorudzebica.getStavke().get(index);
-                
-                if(!stavka.getProizvod().equals(staraStavka.getProizvod()) || stavka.getKolicina() != staraStavka.getKolicina()){
-                    DatabaseBroker.getInstanca().update(stavka);
+
+            Porudzbenica staraPorudzebica = (Porudzbenica) DatabaseBroker.getInstanca().findOne(novaPorudzbenica);
+
+            for (StavkaPorudzbenice stavka : staraPorudzebica.getStavke()) {
+                if (!novaPorudzbenica.getStavke().contains(stavka)) {
+                    DatabaseBroker.getInstanca().delete(stavka);
                 }
             }
-        }
+
+            for (StavkaPorudzbenice stavka : novaPorudzbenica.getStavke()) {
+                if (!staraPorudzebica.getStavke().contains(stavka)) {
+                    DatabaseBroker.getInstanca().insert(stavka);
+                } else {
+                    int index = staraPorudzebica.getStavke().indexOf(stavka);
+                    StavkaPorudzbenice staraStavka = staraPorudzebica.getStavke().get(index);
+
+                    if (!stavka.getProizvod().equals(staraStavka.getProizvod()) || stavka.getKolicina() != staraStavka.getKolicina()) {
+                        DatabaseBroker.getInstanca().update(stavka);
+                    }
+                }
+            }
         } catch (Exception e) {
-                e.printStackTrace();
-                throw new Exception("Sistem ne moze da zapamti porudzbenicu");
+            e.printStackTrace();
+            throw new Exception("Sistem ne moze da zapamti porudzbenicu");
         }
-        
+
         return novaPorudzbenica;
     }
-    
-    
+
 }
